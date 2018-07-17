6a7dffa5c47ea82d78c662468b7f37d255dc393c
Adam Specker <specker@gmail.com>
2018-07-17 14:53:33 -0400

    party data processed from file

--- challenge.js
+++ challenge.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 
-
-const inputArr = fs.readFileSync('input.txt','utf8')
+const partyArr = fs.readFileSync('input.txt','utf8')
     .trim()
     .split('\r\n')
     .map(line=>line.split(' '))
+    .filter(line=>line.includes('party'))
+    .reduce((parties,line)=>{
+        parties.push({
+            name: line[0].slice(0,-1),
+            size: parseInt(line[3],10),
+            dislikes: line[4]==='dislikes'? line.slice(5,line.length) : 'none'
+        })
+        return parties
+    },[])
 
-const tableArr = inputArr.filter(line=>line.includes('tables:'))
-
-const partyArr = inputArr.filter(line=>line.includes('party'))
-
-
-
-console.log(tableArr);
-console.log(partyArr);
+console.log(partyArr)
