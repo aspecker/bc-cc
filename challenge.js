3e911a10bb33eca901bd03b042bd5237b44c417f
Adam Specker <specker@gmail.com>
2018-07-19 11:36:10 -0400

    cleaned output, bugfix seat counter

--- challenge.js
+++ challenge.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('./inputs/overseat.txt','utf8')
+const input = fs.readFileSync('./inputs/dislikeExclusion.txt','utf8')
 // console.log(input)
 
 // proccesses party data into sortable format
@@ -13,13 +13,13 @@ const partyArr = input
         parties.push({
             name: line[0].slice(0,-1),
             size: parseInt(line[3],10),
-            dislikes: line[4]==='dislikes'? line.slice(5,line.length).map(dislike=>dislike.replace(',','')) : 'none',
+            dislikes: line[4]===('dislikes'||'Dislikes')? line.slice(5,line.length).map(dislike=>dislike.replace(',','')) : 'none',
             seated: false
         })
         return parties
     },[])
 
-// proccesses table data into sortable format
+// proccesses table data into sortable format and assigns
 const tableArr = input
     .trim()
     .split('\r\n')
@@ -37,19 +37,18 @@ const tableArr = input
     }, [])
 
 // function to add amount of seats in an array
-function addSeats(array){
-    array.reduce((sum,item)=>{
+function addSeats(array){ 
+    const seatCount = array.reduce((sum,item)=>{
         sum += item.size;
-        console.log(sum);
          return sum
     },0)
+    return seatCount
 }
     
 // function determines if there are enough seats for each guest, and returns false if not
 const checkSeatingAmount = (tables,parties) => {
     const seatCount = addSeats(tables);
     const guestCount = addSeats(parties);
-    console.log(seatCount,guestCount);
     if (guestCount>seatCount){
         return false;
     } 
@@ -66,13 +65,12 @@ const checkLargestTable = (tables,parties) => {
     return true;
 }
 
-// first sort of tables, places parties with exact 
+// first sort of tables, places parties with exact guest count to seat count match
 const sortExactMatch = (tables, parties) => {
     const tableSort = tables;
     const partySort = parties;
     for (let i=0; i <tableSort.length;i+=1){
         for (let j=0; j <partySort.length; j+=1){
-            // console.log(tableSort)
             // first place all parties equal to the size of a table
             if (tableSort[i].size===partySort[j].size && partySort[j].seated===false && tableSort[i].seated+partySort[j].size<=tableSort[i].size){
                 partySort[j].seated=true;
@@ -84,7 +82,7 @@ const sortExactMatch = (tables, parties) => {
     return [tableSort, partySort]
 }
 
-// function to check if a tables existing seated party conflicts with the party to be added
+// function to check if any existing seated parties at a table conflict with the party to be added
 const checkDislikes = (table, party) => {
     const partyString = table.parties.reduce((string,family)=>{
        string += family
@@ -128,11 +126,18 @@ const sortDislikes = (tables,parties) => {
 
 // core function to sort 
 const sortTables = (tables, parties) => {
-    // checking for edge cases
+    // checking if there are enough total seats
     if (checkSeatingAmount(tables,parties)===false){
-        return console.log(`There are not enough seats for every guest.`)
-    } if (checkLargestTable(tables,parties)===false){
-        return console.log(`The largest table cannot seat the largest party.`)
+        return console.log(`    
+        The sort could not be completed because there are not enough total seats for every guest.
+        Please add additional seats or tables and try again.
+        `)
+    } // checking if the largest table can seat the largst party
+    if (checkLargestTable(tables,parties)===false){
+        return console.log(`
+        The largest table cannot seat the largest party.
+        Please increase the size of the largest table.
+        `)
     }
 
     const firstSort = sortExactMatch(tables,parties);
@@ -142,11 +147,25 @@ const sortTables = (tables, parties) => {
     const secondSort = sortDislikes(tableArray,partyArray);
     let escaped;
     [tableArray, partyArray,escaped] = secondSort;
+    
+    // displaying output to the console
+    console.log('\n')
+    console.log(`   Guest List: `)
+    partyArray.map(party=>console.log(`    ${party.name}, party of ${party.size}. ${party.dislikes==='none'?'':`Dislikes ${party.dislikes.join(', ')}` }`))
+    console.log('\n')
+    console.log(`   Seating Arrangement`)
+    tableArray.map(table=>console.log(`    Table ${table.id} (${table.seated}/${table.size} seats filled) is seating ${table.parties.join(', ')}`))
 
-    console.log(`while loop threshold reached?: ${escaped}`);
-    console.log(partyArray);
-    return console.log(tableArray);
+    if (escaped===true){
+        console.log('\n')
+        console.log(`   We were unable to sort all of your guests due to seating preferences or table seating design. \n    Please adjust the amount of seats and/or tables.`)
+        console.log(`   The following parties remain unseated:`)
+        partyArray.filter(party=>party.seated===false).map(party=> console.log(`    ${party.name}, party of ${party.size} ${party.dislikes==='none'?'':`Dislikes ${party.dislikes.join(', ')}` }`))
+    }
+    
+    
+
+    return 'sort done';
 }
-
 
 sortTables(tableArr,partyArr)
